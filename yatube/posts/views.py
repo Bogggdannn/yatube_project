a50bd8d9982c02e49122556068bf8d3cb866ad07
Bogggdannn <b.grishin1>
2023-03-07 19:45:50 +0500

    sixth commit

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from .models import Post,Group
+
 # Create your views here.
 def index(request):
     template = "posts\index.html"
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        "text" : "Это главная страница проекта Yatube"}
+        'posts':posts
+    }
     return render(request,template,context)
 
 def group_posts(request,slug):
     template = "posts\group_list.html"
-    
+    group = get_object_or_404(Group,slug = slug)
+    posts = Post.objects.filter(group = group).order_by('-pub_date')[:10]
     context = {
-        "text" : "Здесь будет информация о группах проекта Yatube"}
+        "posts" : posts,
+        "group" : group
+    }
+    
     return render(request,template,context)
